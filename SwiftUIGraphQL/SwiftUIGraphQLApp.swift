cdab2f2f4ef9def0f033a1a3f69e184f7fa6c220
Jboy422 <gitonly543@gmail.com>
2024-07-06 06:46:36 +0900

    Initial Commit

---
+++ SwiftUIGraphQL/SwiftUIGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGraphQLApp.swift
+//  SwiftUIGraphQL
+//
+//  Created by Jboy422 on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
